f5d57b4a22c862ed3163365f50b73fde96bc5cfc
Dee Dong <d.d02031pw@gmail.com>
2019-05-04 21:29:32 -0700

    modify gitignore

--- wordsense/wordsense/settings.py
+++ wordsense/wordsense/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lcd-wordsense.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
